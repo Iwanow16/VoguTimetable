b6aad7e108c4665df5e7d0711fa9751ae979a009
iwano <iwanov-23@bk.ru>
2025-01-20 12:47:48 +0300

    Удаление ненужных зависимостей

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -47,19 +47,14 @@ dependencies {
     implementation(libs.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
 
-    implementation(project(":domain"))
-
     implementation(libs.androidx.core.ktx)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(project(":domain"))
 }
